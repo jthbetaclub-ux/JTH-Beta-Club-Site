b19c26a5bc28d6d74f2a91c020daefe46f5616bd
stoneclym <133927098+stoneclym@users.noreply.github.com>
2026-05-29 12:05:37 -0400

    Update events from Google Sheets (3 events)

--- events-data.jsx
+++ events-data.jsx
@@ -1,7 +1,17 @@
 // events-data.jsx ? Auto-generated. Do not edit manually.
-// Last updated: 5/29/2026, 12:04:15 PM ET
+// Last updated: 5/29/2026, 12:05:37 PM ET
 
 const UPCOMING_EVENTS = [
+  {
+    "title": "Holly Tree Dance",
+    "day": "31",
+    "mo": "MAY",
+    "detail": "Join Holly Tree Elementary for their school dance!",
+    "spots": "10",
+    "url": "https://www.signupgenius.com/go/904054BAFAD28A1F94-63997107-beta#/",
+    "url2": "https://www.signupgenius.com/go/4090449ADA928A5FC1-63999879-holly#/",
+    "dateNote": ""
+  },
   {
     "title": "Showing coach moody the website",
     "day": "29",
@@ -21,16 +31,6 @@ const UPCOMING_EVENTS = [
     "url": "https://www.signupgenius.com/go/4090449ADA928A5FC1-64077165-wilmington#/",
     "url2": "",
     "dateNote": "Every Saturday 1/24- 5/23"
-  },
-  {
-    "title": "Holly Tree Dance",
-    "day": "31",
-    "mo": "MAY",
-    "detail": "Join Holly Tree Elementary for their school dance!",
-    "spots": "10",
-    "url": "https://www.signupgenius.com/go/904054BAFAD28A1F94-63997107-beta#/",
-    "url2": "https://www.signupgenius.com/go/4090449ADA928A5FC1-63999879-holly#/",
-    "dateNote": ""
   }
 ];
 
