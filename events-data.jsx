df1605b54b2ef6e79759930876282bf332b4a45b
jthbetaclub-ux <jthbetaclub@gmail.com>
2026-05-29 13:05:27 -0400

    Update events (3 one-time, 2 recurring)

--- events-data.jsx
+++ events-data.jsx
@@ -1,11 +1,11 @@
 // events-data.jsx ? Auto-generated. Do not edit manually.
-// Last updated: 5/29/2026, 12:08:39 PM ET
+// Last updated: 5/29/2026, 1:05:27 PM ET
 
 const UPCOMING_EVENTS = [
   {
     "title": "Wilmington Municipal Golf Course Athletic Event",
-    "day": "16",
-    "mo": "MAY",
+    "day": "24",
+    "mo": "JAN",
     "detail": "This event is celebrating the history of Wilmington athletics!",
     "spots": "8",
     "url": "https://www.signupgenius.com/go/4090449ADA928A5FC1-64077165-wilmington#/",
@@ -35,7 +35,22 @@ const UPCOMING_EVENTS = [
 ];
 
 const RECURRING_EVENTS = [
-  // example shape: { title, dates, detail, spots, url, url2 }
+  {
+    "title": "Healthy Kids Running Series",
+    "dates": "4/12, 4/19, 4/26, 5/03,  5/10, 5/24",
+    "detail": "run",
+    "spots": "20",
+    "url": "apple.com",
+    "url2": "target.com"
+  },
+  {
+    "title": "Hang out with Ford",
+    "dates": "6/32, 7/84",
+    "detail": "Hang out with ford cause he has no friends",
+    "spots": "10",
+    "url": "https://cabletwo.net/horsle/",
+    "url2": "https://www.nytimes.com/games/wordle/index.html"
+  }
 ];
 
 Object.assign(window, { UPCOMING_EVENTS, RECURRING_EVENTS });
