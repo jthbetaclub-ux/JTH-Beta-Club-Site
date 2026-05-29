65e418917b96380491957a51eebbed61005a1606
stoneclym <133927098+stoneclym@users.noreply.github.com>
2026-05-29 12:08:39 -0400

    Update events from Google Sheets (3 events)

--- events-data.jsx
+++ events-data.jsx
@@ -1,36 +1,36 @@
 // events-data.jsx ? Auto-generated. Do not edit manually.
-// Last updated: 5/29/2026, 12:07:54 PM ET
+// Last updated: 5/29/2026, 12:08:39 PM ET
 
 const UPCOMING_EVENTS = [
   {
     "title": "Wilmington Municipal Golf Course Athletic Event",
-    "day": "Sat May 16 2026 00:00:00 GMT-0400 (Eastern Daylight Time)",
-    "mo": "16",
-    "detail": "May",
-    "spots": "This event is celebrating the history of Wilmington athletics!",
-    "url": "8",
-    "url2": "https://www.signupgenius.com/go/4090449ADA928A5FC1-64077165-wilmington#/",
-    "dateNote": ""
+    "day": "16",
+    "mo": "MAY",
+    "detail": "This event is celebrating the history of Wilmington athletics!",
+    "spots": "8",
+    "url": "https://www.signupgenius.com/go/4090449ADA928A5FC1-64077165-wilmington#/",
+    "url2": "",
+    "dateNote": "Every Saturday 1/24- 5/23"
   },
   {
     "title": "Showing coach moody the website",
-    "day": "Fri May 29 2026 00:00:00 GMT-0400 (Eastern Daylight Time)",
-    "mo": "29",
-    "detail": "May",
-    "spots": "Make sure he likes it",
-    "url": "2",
-    "url2": "https://www.apple.com/",
-    "dateNote": "https://www.target.com/"
+    "day": "29",
+    "mo": "MAY",
+    "detail": "Make sure he likes it",
+    "spots": "2",
+    "url": "https://www.apple.com/",
+    "url2": "https://www.target.com/",
+    "dateNote": ""
   },
   {
     "title": "Holly Tree Dance",
-    "day": "Sun May 31 2026 00:00:00 GMT-0400 (Eastern Daylight Time)",
-    "mo": "31",
-    "detail": "May",
-    "spots": "Join Holly Tree Elementary for their school dance!",
-    "url": "10",
-    "url2": "https://www.signupgenius.com/go/904054BAFAD28A1F94-63997107-beta#/",
-    "dateNote": "https://www.signupgenius.com/go/4090449ADA928A5FC1-63999879-holly#/"
+    "day": "31",
+    "mo": "MAY",
+    "detail": "Join Holly Tree Elementary for their school dance!",
+    "spots": "10",
+    "url": "https://www.signupgenius.com/go/904054BAFAD28A1F94-63997107-beta#/",
+    "url2": "https://www.signupgenius.com/go/4090449ADA928A5FC1-63999879-holly#/",
+    "dateNote": ""
   }
 ];
 
