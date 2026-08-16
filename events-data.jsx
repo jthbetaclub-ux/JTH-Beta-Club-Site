6ab652483cf1afe3c1ea8400b30812246ab5786c
jthbetaclub-ux <jthbetaclub@gmail.com>
2026-08-16 18:54:08 -0400

    Update events (1 one-time, 0 recurring)

--- events-data.jsx
+++ events-data.jsx
@@ -1,17 +1,7 @@
 // events-data.jsx ? Auto-generated. Do not edit manually.
-// Last updated: 8/16/2026, 6:18:19 PM ET
+// Last updated: 8/16/2026, 6:54:08 PM ET
 
 const UPCOMING_EVENTS = [
-  {
-    "title": "Back to School Block Party",
-    "day": "15",
-    "mo": "AUGUST",
-    "detail": "Trinity United Methodist Church needs your help setting up for and cleaning up from their Fifth Annual Back-To-School Block Party.",
-    "spots": "63",
-    "url": "https://www.signupgenius.com/go/30E0D4BA4AA2EA4FE3-61748391-2026",
-    "url2": "https://www.signupgenius.com/go/10C094EA9A72DA5FACE9-64972700-trinity#/",
-    "dateNote": ""
-  },
   {
     "title": "Teacher Classroom Help",
     "day": "17, 18",
