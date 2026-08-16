85153ae158fb248d8f241a7581a61c1a492b1245
jthbetaclub-ux <jthbetaclub@gmail.com>
2026-08-16 18:17:06 -0400

    Update events (2 one-time, 0 recurring)

--- events-data.jsx
+++ events-data.jsx
@@ -1,5 +1,5 @@
 // events-data.jsx ? Auto-generated. Do not edit manually.
-// Last updated: 8/1/2026, 2:40:43 PM ET
+// Last updated: 8/16/2026, 6:17:06 PM ET
 
 const UPCOMING_EVENTS = [
   {
@@ -10,6 +10,16 @@ const UPCOMING_EVENTS = [
     "spots": "63",
     "url": "https://www.signupgenius.com/go/30E0D4BA4AA2EA4FE3-61748391-2026",
     "url2": "https://www.signupgenius.com/go/10C094EA9A72DA5FACE9-64972700-trinity#/",
+    "dateNote": ""
+  },
+  {
+    "title": "Teacher Classroom Help",
+    "day": "17",
+    "mo": "AUGUST",
+    "detail": "Help teachers in their classrooms!",
+    "spots": "15",
+    "url": "https://www.signupgenius.com/go/10C094EAAAF2DA4FECF8-65200269-teacher",
+    "url2": "",
     "dateNote": ""
   }
 ];
