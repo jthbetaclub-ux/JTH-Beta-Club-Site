b64070082e237a0f6dd34dc23a60919088af244f
jthbetaclub-ux <jthbetaclub@gmail.com>
2026-08-17 11:51:01 -0400

    Update events (2 one-time, 0 recurring)

--- events-data.jsx
+++ events-data.jsx
@@ -1,5 +1,5 @@
 // events-data.jsx ? Auto-generated. Do not edit manually.
-// Last updated: 8/16/2026, 6:54:08 PM ET
+// Last updated: 8/17/2026, 11:51:00 AM ET
 
 const UPCOMING_EVENTS = [
   {
@@ -9,6 +9,16 @@ const UPCOMING_EVENTS = [
     "detail": "Help teachers in their classrooms!",
     "spots": "15",
     "url": "https://www.signupgenius.com/go/10C094EAAAF2DA4FECF8-65200269-teacher",
+    "url2": "",
+    "dateNote": ""
+  },
+  {
+    "title": "Freshman Laptop Assistance",
+    "day": "19",
+    "mo": "AUGUST",
+    "detail": "Help Hoggard staff organize laptops!",
+    "spots": "8",
+    "url": "https://www.signupgenius.com/go/10C094EA9A72DA5FACE9-65210980-freshman",
     "url2": "",
     "dateNote": ""
   }
