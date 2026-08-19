c72da27f513bc45449abb9d6d24bbcca2d8fc9d5
jthbetaclub-ux <jthbetaclub@gmail.com>
2026-08-19 11:10:01 -0400

    Update events (1 one-time, 1 recurring)

--- events-data.jsx
+++ events-data.jsx
@@ -1,17 +1,7 @@
 // events-data.jsx ? Auto-generated. Do not edit manually.
-// Last updated: 8/19/2026, 11:04:05 AM ET
+// Last updated: 8/19/2026, 11:10:00 AM ET
 
 const UPCOMING_EVENTS = [
-  {
-    "title": "Pine Valley Methodist Church 49ers",
-    "day": "18",
-    "mo": "AUGUST",
-    "detail": "Volunteers will help serve a meal, set up, clean up, wash dishes, and assist attendees to their tables.",
-    "spots": "10",
-    "url": "https://www.signupgenius.com/go/4090449ADA928A5FC1-65222472-pine",
-    "url2": "",
-    "dateNote": ""
-  },
   {
     "title": "Freshman Laptop Assistance",
     "day": "19",
@@ -20,16 +10,6 @@ const UPCOMING_EVENTS = [
     "spots": "8",
     "url": "https://www.signupgenius.com/go/10C094EA9A72DA5FACE9-65210980-freshman",
     "url2": "",
-    "dateNote": ""
-  },
-  {
-    "title": "Pumpkin Patch",
-    "day": "23",
-    "mo": "OCTOBER",
-    "detail": "Help that church sort pumpkins!",
-    "spots": "30",
-    "url": "https://www.signupgenius.com/go/4090449ADA928A5FC1-65222472-pine#/",
-    "url2": "https://www.target.com/",
     "dateNote": ""
   }
 ];
